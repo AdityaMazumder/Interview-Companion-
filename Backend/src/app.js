2baeed1224c54bcfb9a9e34e31ee34c340aed330
Aditya Mazumder <147578044+AdityaMazumder@users.noreply.github.com>
2026-08-19 21:55:01 +0530

    Refactor CORS settings for production environment
    Updated CORS configuration to use environment variable for production.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,24 +1,4 @@
-// const express = require("express")
-// const cookieParser = require("cookie-parser")
-// const cors = require("cors")
 
-// const app = express()
-
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
-
-// /* require all the routes here */
-// const authRouter = require("./routes/auth.routes")
-// const interviewRouter = require("./routes/interview.routes")
-
-
-// /* using all the routes here */
-// app.use("/api/auth", authRouter)
-// app.use("/api/interview", interviewRouter)
 
 
 
@@ -34,7 +14,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" 
+        ? "https://ai-interviewer-k4pc.onrender.com" 
+        : "http://localhost:5173",
     credentials: true
 }))
 
