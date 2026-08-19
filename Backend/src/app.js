ee8ab3ac40636f0f158f5c79caefb1f0346f3980
Aditya Mazumder <adityamazumder93537@gmail.com>
2026-08-19 21:15:53 +0530

    Preparing deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,33 @@
+// const express = require("express")
+// const cookieParser = require("cookie-parser")
+// const cors = require("cors")
+
+// const app = express()
+
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
+
+// /* require all the routes here */
+// const authRouter = require("./routes/auth.routes")
+// const interviewRouter = require("./routes/interview.routes")
+
+
+// /* using all the routes here */
+// app.use("/api/auth", authRouter)
+// app.use("/api/interview", interviewRouter)
+
+
+
+// module.exports = app
+
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
@@ -15,11 +42,14 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-
+/* serve frontend build (single combined deployment) */
+app.use(express.static(path.join(__dirname, "../../Frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"))
+})
 
 module.exports = app
